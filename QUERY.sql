7bbac6cc4ce6d21c6b840709332514e7ae882238
Diganta999 <digantasikder123@gmail.com>
2026-06-15 17:36:23 +0600

    create matches table

--- QUERY.sql
+++ QUERY.sql
@@ -1,7 +1,7 @@
 --FOOTBALL TICKET BOOKING SYSTEM 
 
 
---1. USERS TABLE 
+--1. CREATE USERS TABLE 
 
 CREATE TABLE users(
     user_id SERIAL PRIMARY KEY ,
@@ -11,3 +11,14 @@ CREATE TABLE users(
     phone_number TEXT
 );
 
+--2. CREATE MATCH TABLE
+
+CREATE TABLE matches(
+    match_id SERIAL PRIMARY KEY,
+    fixture VARCHAR(150) NOT  NULL,
+    tournament_category VARCHAR(100) NOT NULL,
+    base_ticket_price DECIMAL(10,2) NOT NULL,
+    match_status VARCHAR(30) NOT NULL,
+    CHECK(base_ticket_price>=0),
+    CHECK(match_status IN ('Available','Selling fast','Sold Out','Postponed'))
+);
