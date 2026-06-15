1ec4fc9f3e32b762f0badd450cef639bb194d824
Diganta999 <digantasikder123@gmail.com>
2026-06-15 19:02:26 +0600

    quary 1 ,2 ,3 done

--- QUERY.sql
+++ QUERY.sql
@@ -46,7 +46,9 @@ VALUES('Diganta Sikder','digantasikder123@gmail.com','FootBall Fan','01308340811
 ('Shasank Sikder','shasanksikder088@gmail.com','FootBall Fan','01742814803'),
 ('Jharna Rani Sikder','jharnasikder088@gmail.com','Ticket Manager','01811387952'),
 ('Shipan Sikder ','shipansikder123@gmail.com','FootBall Fan','01610673585'),
-('Shuchitra Sikder','shuchitrasikder123@gmail.com','FootBall Fan',NULL);
+('Shuchitra Sikder','shuchitrasikder123@gmail.com','FootBall Fan',NULL)
+('Tanvir Haque','tanvir@gmail.com','FootBall Fan','01915154224')
+;
 
 --2. INSERT MATCHES DATA 
 INSERT INTO matches(fixture,tournament_category,base_ticket_price,match_status)
@@ -54,4 +56,36 @@ VALUES ('Bangladesh vs India','SAFF Championship','500.00','Available'),
 ('Brazil vs Argentina','International Friendly',1500.00,'Selling fast'),
 ('Real Madrid vs Barcelona','La Liga',3000.00,'Sold Out'),
 ('Manchester City vs Liverpool','Premier League',2500.00,'Available'),
-('Bayern Munich vs Dortmund','Bundesliga',2000.00,'Postponed');
+('Bayern Munich vs Dortmund','Bundesliga',2000.00,'Postponed')
+('PSG vs Arsenal','Champions League',3500.00,'Available');
+
+
+--3. INSERT BOOKING DATA 
+
+
+INSERT INTO Bookings(user_id,match_id,seat_number,payment_status,total_cost)
+VALUES(1,1,'A01','Confirmed',500.00),
+(2,2,'B05','Pending',1500.00),
+(4,4,'C10','Confirmed',2500.00),
+(1,3,'D15','Cancelled',3000.00),
+(2,5,'E20','Refunded',2000.00)
+(3,2,'F12',NULL,1500.00);
+
+
+--QUERY 1 
+
+SELECT match_id, fixture, base_ticket_price FROM matches WHERE tournament_category ='Champions League' AND match_status ='Available';
+
+
+
+--QUERY 2 
+
+SELECT user_id,full_name,email FROM users WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%';
+
+
+
+--QUERY 3 
+
+SELECT booking_id,user_id,match_id,COALESCE(payment_status,'Action Required') AS systematic_status FROM Bookings WHERE payment_status IS NULL;
+
+
