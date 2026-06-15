ef14ead7870e68636de76f10f88c435e5bc9207f
Diganta999 <digantasikder123@gmail.com>
2026-06-15 17:13:46 +0600

    create users table

--- QUERY.sql
+++ QUERY.sql
@@ -1,2 +1,13 @@
+--FOOTBALL TICKET BOOKING SYSTEM 
 
+
+--1. USERS TABLE 
+
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY ,
+    full_name  VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    role VARCHAR(20) NOT NULL CHECK(role IN ('Ticket Manager','FootBall Fan')),
+    phone_number TEXT
+);
 
